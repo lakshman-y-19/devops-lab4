cdf4b48731cde1a56c910750fccc77a34ef0c0b1
lakshman-y-19 <75108128+lakshman-y-19@users.noreply.github.com>
2025-07-06 22:59:35 +0530

    Update#2 App.java
    Second update for CI check

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI!");
+        System.out.println("Second update from CI test");
     }
 }
